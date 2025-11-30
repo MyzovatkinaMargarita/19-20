555bf312a02e03570a6fe82e7e8199252bae1945
@Margarita52 <muzovatkinamargarita@gmail.com>
2025-12-01 00:41:13 +0300

    icons.tsx

---
+++ src/icons/icons.tsx
@@ -0,0 +1,9 @@
+export function LinkIcon() {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+         xmlns="http://www.w3.org/2000/svg">
+      <path d="M9.1718 14.8234L14.8287 9.16655M7.05086 11.2876L5.63664 12.7019C4.07455 14.264 4.07409 16.7968 5.63619 18.3589C7.19829 19.921 9.7317 19.9205 11.2938 18.3584L12.7065 16.9444M11.2929 7.04463L12.7071 5.63042C14.2692 4.06832 16.8016 4.0686 18.3637 5.6307C19.9258 7.19279 19.9257 9.72548 18.3636 11.2876L16.9501 12.7017"
+            stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+}
